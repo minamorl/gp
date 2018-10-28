dab364ee9caa55614646b57a6e46edf758deb7b8
minamorl <minamorl@minamorl.com>
2018-10-28 11:12:18 +0900

    Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello World" << std::endl;
+  GLFWwindow* window;
+  if(!glfwInit()) return -1;
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+
   return 0;
 }
